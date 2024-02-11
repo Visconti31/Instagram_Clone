5daa05777ae1a17c04368cc7db43e6c0743a8096
unknown <visconti.joao@gmail.com>
2024-02-11 15:55:15 +0000

    Design the Auth page and handle toggle between register and login

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,7 +1,30 @@
+import { Flex, Container, Box, Image, VStack } from '@chakra-ui/react'
 import React from 'react'
+import AuthForm from '../components/AuthForm/AuthForm'
 
 const AuthPage = () => {
-  return <div>AuthPage</div>
+  return (
+    <Flex minH={'100vh'} justifyContent={'center'} alignItems={'center'} px={4}>
+      <Container maxW={'container.md'} padding={0}>
+        <Flex justifyContent={'center'} alignItems={'center'} gap={10}>
+          {/* Left hand-side */}
+          <Box display={{ base: 'none', md: 'block' }}>
+            <Image src='/auth.png' h={650} alt='Phone Image' />
+          </Box>
+
+          {/* Left hand-side */}
+          <VStack spacing={4} align={'stretch'}>
+            <AuthForm />
+            <Box textAlign={'center'}>Get app now</Box>
+            <Flex gap={5} justifyContent={'center'}>
+              <Image src='/playstore.png' h={10} alt='PlayStore Logo' />
+              <Image src='/microsoft.png' h={10} alt='Microsoft Logo' />
+            </Flex>
+          </VStack>
+        </Flex>
+      </Container>
+    </Flex>
+  )
 }
 
 export default AuthPage
